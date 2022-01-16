4ec282b14b49b02fb0497e795f634c1cf8875734
Michal Kurzal <michalkurzal200@gmail.com>
2022-01-16 06:40:17 +0100

    dashboard component

--- componets/countriesList.js
+++ componets/countriesList.js
@@ -1,5 +1,5 @@
 import React, {useMemo} from 'react';
-import {ScrollView, Text,  Image, TouchableOpacity} from 'react-native';
+import {ScrollView, Text, Image, TouchableOpacity} from 'react-native';
 import {useTheme} from '@react-navigation/native';
 import images from '../img/48/images';
 var codes = require('../codes.json');
@@ -13,9 +13,19 @@ export function ListComponent() {
                 return (
                     <TouchableOpacity
                         key={country.alpha}
-                        style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            paddingLeft: 5,
+                            paddingRight: 5,
+                        }}>
                         <Image source={images[country.name]}></Image>
-                        <Text style={{color: colors.text, textAlignVertical: 'center'}}>
+                        <Text
+                            style={{
+                                color: colors.text,
+                                textAlignVertical: 'center',
+                                fontFamily: 'monospace',
+                            }}>
                             {country.name}
                         </Text>
                     </TouchableOpacity>
--- componets/dashboard.js
+++ componets/dashboard.js
@@ -1,17 +1,64 @@
-import React, {useEffect} from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {useTheme} from '@react-navigation/native';
 import getWorldwideData from '../api/getWorldwideData';
 
 export function Dashboard() {
+    const {colors} = useTheme();
+
+    const [cases, setCases] = useState(0);
+    const [deaths, setDeaths] = useState(0);
+    const [recovered, setRecovered] = useState(0);
+    const [todayCases, setTodayCases] = useState(0);
+    const [todayDeaths, setTodayDeaths] = useState(0);
+    const [todayRecovered, setTodayRecovered] = useState(0);
+
+    const styles = StyleSheet.create({
+        text: {
+            color: colors.text,
+            fontSize: 16,
+            fontFamily: 'monospace',
+        },
+        listRow: {
+            flex: 1,
+            justifyContent: 'space-around',
+            flexDirection: 'row',
+        },
+    });
+
     useEffect(() => {
         getWorldwideData().then(res => {
-            console.log(res);
+            setCases(res['cases']);
+            setDeaths(res['deaths']);
+            setRecovered(res['recovered']);
+            setTodayCases(res['todayCases']);
+            setTodayDeaths(res['todayDeaths']);
+            setTodayRecovered(res['todayRecovered']);
         });
     }, []);
 
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Dashboard</Text>
+        <View style={{flex: 0.2, justifyContent: 'space-between'}}>
+            <View style={styles.listRow}>
+                <Text style={styles.text}>Total Cases</Text>
+                <Text style={styles.text}>Total Deaths</Text>
+                <Text style={styles.text}>Total Recovered</Text>
+            </View>
+            <View style={styles.listRow}>
+                <Text style={styles.text}>{cases.toLocaleString('fr-FR')}</Text>
+                <Text style={styles.text}>{deaths.toLocaleString('fr-FR')}</Text>
+                <Text style={styles.text}>{recovered.toLocaleString('fr-FR')}</Text>
+            </View>
+            <View style={styles.listRow}>
+                <Text style={styles.text}>Today Cases</Text>
+                <Text style={styles.text}>Today Deaths</Text>
+                <Text style={styles.text}>Today Recovered</Text>
+            </View>
+            <View style={styles.listRow}>
+                <Text style={styles.text}>{todayCases.toLocaleString('fr-FR')}</Text>
+                <Text style={styles.text}>{todayDeaths.toLocaleString('fr-FR')}</Text>
+                <Text style={styles.text}>{todayRecovered.toLocaleString('fr-FR')}</Text>
+            </View>
         </View>
     );
 }
