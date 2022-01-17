753e7dbc003127d940ef82a6e15976f6a0e5a17f
Michal Kurzal <michalkurzal200@gmail.com>
2022-01-17 13:12:36 +0100

    ReadMe file

--- componets/countriesList.js
+++ componets/countriesList.js
@@ -1,5 +1,5 @@
-import React, {useMemo} from 'react';
-import {ScrollView, Text, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {Text, Image, TouchableOpacity, FlatList, View} from 'react-native';
 import {useTheme} from '@react-navigation/native';
 import images from '../img/48/images';
 var codes = require('../codes.json');
@@ -7,31 +7,34 @@ var codes = require('../codes.json');
 export function ListComponent() {
     const {colors} = useTheme();
 
-    const Countries = useMemo(
-        () =>
-            codes.map(country => {
-                return (
-                    <TouchableOpacity
-                        key={country.alpha}
-                        style={{
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                            paddingLeft: 5,
-                            paddingRight: 5,
-                        }}>
-                        <Image source={images[country.name]}></Image>
-                        <Text
-                            style={{
-                                color: colors.text,
-                                textAlignVertical: 'center',
-                                fontFamily: 'monospace',
-                            }}>
-                            {country.name}
-                        </Text>
-                    </TouchableOpacity>
-                );
-            }),
-        [colors],
+    const Country = ({item}) => (
+        <TouchableOpacity
+            key={item.alpha}
+            style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                paddingLeft: 5,
+                paddingRight: 5,
+            }}>
+            <Image source={images[item.name]}></Image>
+            <Text
+                style={{
+                    color: colors.text,
+                    textAlignVertical: 'center',
+                    fontFamily: 'monospace',
+                }}>
+                {item.name}
+            </Text>
+        </TouchableOpacity>
     );
-    return <ScrollView>{Countries}</ScrollView>;
+
+    const renderItem = ({item}) => {
+        return <Country item={item} />;
+    };
+
+    return (
+        <View>
+            <FlatList data={codes} renderItem={renderItem} />
+        </View>
+    );
 }
