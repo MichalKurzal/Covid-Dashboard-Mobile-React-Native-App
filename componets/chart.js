f04dda4d146f2e22f44fab9062fae3dc151a24d1
Michal Kurzal <michalkurzal200@gmail.com>
2022-02-06 02:58:02 +0100

    react-native-svg installation

---
+++ componets/chart.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Svg} from 'react-native-svg';
+
+export const Chart = () => {
+    return <Svg width={300} height={300}></Svg>;
+};
--- componets/historicalData.js
+++ componets/historicalData.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 import {useTheme} from '@react-navigation/native';
 import getHistoricalData from '../api/getHistroricalData';
+import {Chart} from './chart';
 
 const HistoricalData = () => {
     const {colors} = useTheme();
@@ -14,6 +15,7 @@ const HistoricalData = () => {
     return (
         <View style={{alignItems: 'center'}}>
             <Text style={{color: colors.text}}>Data chart</Text>
+            <Chart />
         </View>
     );
 };
